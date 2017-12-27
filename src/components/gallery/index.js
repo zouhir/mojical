757e7d31646641b66b8a2d3da10cdcdccd26d3ca
Zouhir <zouhir@zouhir.org>
2017-12-28 00:10:25 +1100

    design & ui

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,9 +1,29 @@
 import { h, Component } from "preact";
-
+import cx from "classnames";
 import style from "./style.scss";
 
-const componentName = () => {
-  return <div className={style.gallery}>ss</div>;
-};
+class Gallery extends Component {
+  state = {
+    img: null,
+    loaded: false
+  };
+  componentDidMount() {
+    this.setState({
+      img:
+        "data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAANAA4DASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAABAUH/8QAIBAAAgEDBQEBAAAAAAAAAAAAAQIDBBEhAAUGEjEUUf/EABUBAQEAAAAAAAAAAAAAAAAAAAID/8QAGhEAAgIDAAAAAAAAAAAAAAAAABEBEgIhYf/aAAwDAQACEQMRAD8AzXj9e+7wRPO7xAC7LEwUyG9uoYggfudQuQcgmpZRHShWHZh2fOAbDRqBpKSJY4nPQZsfDm+hbhB9UgaVzcX8wPb6qsrcEoq3s//Z"
+    });
+    let newSrc = "/assets/sample-bg.jpg";
+    this.setState({
+      img: newSrc,
+      loaded: true
+    });
+  }
+  render({}, { img, loaded }) {
+    let classes = cx(style.gallery, loaded && style.loaded);
+    return (
+      <div className={classes} style={{ backgroundImage: `url(${img})` }} />
+    );
+  }
+}
 
-export default componentName;
+export default Gallery;
