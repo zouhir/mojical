cb4d1179822ad4df299decc0ceddb120f1b88517
Zouhir <zouhir@zouhir.org>
2017-12-27 23:29:43 +1100

    env config fix fix

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,6 @@
 module.exports = (config, env, helpers) => {
   let definePlugin = helpers.getPluginsByName(config, "DefinePlugin")[0];
   let nodeEnvPrd = process.env.NODE_ENV === "production";
-  console.log(nodeEnv);
   definePlugin.plugin["definitions"]["MOJICAL_ENV"] = nodeEnvPrd
     ? `"production"`
     : `"staging"`;
